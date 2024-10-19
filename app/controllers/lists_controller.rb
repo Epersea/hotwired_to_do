0f0166b7199a8648f69cb4617271595f9445f5e5
elisafvic <elisapersea@gmail.com>
2024-10-19 11:08:12 +0200

    Create

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,4 +7,15 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
+
+  def create
+    list = List.create(list_params)
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title)
+  end
 end
