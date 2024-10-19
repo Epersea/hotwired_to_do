242787251a5313ffc7ce89e3c6eb41fd4335d59e
elisafvic <elisapersea@gmail.com>
2024-10-19 11:59:45 +0200

    Edit and update

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,13 +10,25 @@ class ListsController < ApplicationController
 
   def create
     list = List.create(list_params)
+    
+    redirect_to lists_path
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+
     redirect_to lists_path
   end
 
   def destroy
     list = List.find(params[:id])
     list.destroy!
-    
+
     redirect_to lists_path
   end
 
